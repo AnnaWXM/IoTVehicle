1b8dd564a9ef9906e665f08398723542fd7d16be
AnnaWXM <xiaomininfi@gmail.com>
2022-11-11 10:35:42 +0200

    make it work

--- 30cm/30cm.ino
+++ 30cm/30cm.ino
@@ -1,6 +1,4 @@
 #include <LiquidCrystal.h>
-//#include <TimerOne.h>
-#include <Wire.h>
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
@@ -18,35 +16,21 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 #define Motor_L_pwm_pin       9
 #define Motor_R_pwm_pin       10
 
-#define CMPS14_address 0x60            //define compass
-  byte raw;
-  int bearing=1;
-  int b_1;
-
-int count_L=0;
-int count_R=0;
+double count_L = 0 , count_R = 0;
 // math for counting distance
-int r = 3;                                        // 3 cm
+int r = 3; // 3 cm
 const float pi = 3.14;
-//int t=0;                                            //time, unit in s;
-float D = 2 * pi * r;
-  // here is a small experiment, when this car have full speed, the distance in 254 pluse is 16cm.  week 6
-  //float p = 254 * D / 16;                           //how many pulse per round when the wheel move.
-float p = 170;              //? ?????
-double DistancePerPulse = 0.063;
+float p = 270;              //? ?????170
+double DistancePerPulse = 0.069;
+double DPulse=1;
 double distance_L = 0;                                //distance in cm
 double distance_R = 0;
 
+
 void isr_L();
 void isr_R();
-//int Timer(int);
-void forward();
-void backward();
-void left();
-void right();
-void stop();
 
-int x,y,xp,xd,ySpeed,yd,pwm_L, pwm_R;                              
+int x,y,xp,xd,ySpeed,yd,pwm_L, pwm_R;   
 
 void setup()
 {
@@ -63,8 +47,6 @@ void setup()
   Serial.begin(9600);
   Serial.println("SMS Messages Receiver");
   
-  pinMode(JoyX,INPUT);  //X
-  pinMode(JoyY,INPUT);  //Y
   pinMode(Motor_R_dir_pin,OUTPUT); //PWM
   pinMode(Motor_L_dir_pin,OUTPUT); //PWM
   pinMode(Motor_L_pwm_pin,OUTPUT); //PWM
@@ -72,18 +54,6 @@ void setup()
 
   attachInterrupt(digitalPinToInterrupt(ENC_B_L), isr_L, FALLING);
   attachInterrupt(digitalPinToInterrupt(ENC_B_R), isr_R, FALLING);
-  // Timer1.initialize( 1000000 );           //initialize timer1, and set a 1s period
-  // Timer1.attachInterrupt( Timer ); 
-  Wire.begin();
-  Wire.beginTransmission(CMPS14_address);            //use compass meature degree
-  Wire.write(1);
-  Wire.endTransmission(false);
-  Wire.requestFrom(CMPS14_address,1,true);
-  if(Wire.available()>=1){
-    raw = Wire.read();
-  }
-  bearing = map(raw,0,255,0,365);
-  b_1 = bearing; 
 }
 
 void loop()                    
@@ -93,149 +63,54 @@ void loop()
   pwm_L=0, pwm_R=0;                       
   xd = map(x, 0, 1023, -10, 10);                     // x dirction
   ySpeed = map(y, 0, 1023, -255, 255);               // y speed, unit in Hz, 1 Hz is 6.28 rad/s, 6.28 Radians in a full circle
-  
-  if(y<550){                          // y dirction
-    yd=0;
-  }else if (y>600){
-    yd=1;
+
+  //distance_L = count_L * DistancePerPulse ;
+  //distance_R = count_R * DistancePerPulse ;
+
+
+
+  while (count_L*DistancePerPulse<10) {                    //forward 10cm
+
+  Serial.println(count_L*DistancePerPulse);
+    digitalWrite(Motor_L_dir_pin, 1);
+    analogWrite(Motor_L_pwm_pin, 254);
+    digitalWrite(Motor_R_dir_pin, 1);
+    analogWrite(Motor_R_pwm_pin, 254);
   }
 
-  if(ySpeed!=-1){
-    digitalWrite(Motor_R_dir_pin,yd);  
-    digitalWrite(Motor_L_dir_pin,yd);
-    if(ySpeed>0){
-      pwm_L=ySpeed;
-      pwm_R=ySpeed;
-    }
-    if(ySpeed<0){
-      pwm_L=-ySpeed;
-      pwm_R=-ySpeed;
-    }
-    if(xd<-1){
-     pwm_L=pwm_L;
-     pwm_R=pwm_R/2;
-    }else if(xd>0){
-     pwm_R=pwm_R;
-     pwm_L=pwm_L/2;
-    }  
-    analogWrite(Motor_L_pwm_pin,pwm_L);
-    analogWrite(Motor_R_pwm_pin,pwm_R);
+    analogWrite(Motor_L_pwm_pin, 0);
+    analogWrite(Motor_R_pwm_pin, 0);
+    delay(500);
+
+   count_L = 0;
+    Serial.println(count_L);
+
+  while (count_L*DistancePerPulse<33) {                                        //turn 180 
+
+    Serial.println(count_L*DistancePerPulse);
+    digitalWrite(Motor_L_dir_pin, 1);
+    analogWrite(Motor_L_pwm_pin, 254);
+    digitalWrite(Motor_R_dir_pin, 1);
+    analogWrite(Motor_R_pwm_pin, 0);
   }
 
-  distance_L = count_L * DistancePerPulse;
-  distance_R = count_R * DistancePerPulse;
+   analogWrite(Motor_L_pwm_pin, 0);
+    analogWrite(Motor_R_pwm_pin, 0);
+    delay(5000);
 
-  lcd.setCursor(0, 0);
-  lcd.print("for left: ");
-  lcd.setCursor(0, 1);
-  lcd.print(distance_L);
-  lcd.setCursor(0, 2);
-  lcd.print("for right: ");
-  lcd.setCursor(0, 3);
-  lcd.print(distance_R);
-
-  Wire.beginTransmission(CMPS14_address);            //use compass meature degree
-  Wire.write(1);
-  Wire.endTransmission(false);
-  Wire.requestFrom(CMPS14_address,1,true);
-  if(Wire.available()>=1){
-    raw = Wire.read();
-  }
-  bearing = map(raw,0,255,0,365);
-  
-  if (distance_L<30) {
-    digitalWrite(Motor_L_dir_pin, HIGH);
-    analogWrite(Motor_L_pwm_pin, HIGH);
-    digitalWrite(Motor_R_dir_pin, HIGH);
-    digitalWrite(Motor_R_pwm_pin, HIGH);
-    }
-  if (distance_L>=30) {
-    digitalWrite(Motor_L_dir_pin, LOW);
-    digitalWrite(Motor_L_pwm_pin, LOW);
-    digitalWrite(Motor_R_dir_pin, LOW);
-    digitalWrite(Motor_R_pwm_pin, LOW);
-    }
-
-  //delay(1000);
-  
-  digitalWrite(Motor_L_dir_pin, LOW);
-  digitalWrite(Motor_L_pwm_pin, HIGH);
-  digitalWrite(Motor_R_dir_pin, HIGH);
-  digitalWrite(Motor_R_pwm_pin, LOW);                    //OR right();
-
-  if( bearing >= b_1 + 180) {
-    digitalWrite(Motor_L_dir_pin, LOW);
-    digitalWrite(Motor_L_pwm_pin, LOW);
-    digitalWrite(Motor_R_dir_pin, LOW);
-    digitalWrite(Motor_R_pwm_pin, LOW);
-    }
-
-  //delay(1000);
-
-  digitalWrite(Motor_L_dir_pin, HIGH);
-  digitalWrite(Motor_L_pwm_pin, LOW);
-  digitalWrite(Motor_R_dir_pin, LOW);
-  digitalWrite(Motor_R_pwm_pin, HIGH);
-
-  if( bearing == b_1) {
-    digitalWrite(Motor_L_dir_pin, LOW);
-    digitalWrite(Motor_L_pwm_pin, LOW);
-    digitalWrite(Motor_R_dir_pin, LOW);
-    digitalWrite(Motor_R_pwm_pin, LOW);
-    }
-
-  digitalWrite(Motor_L_dir_pin, LOW);
-  digitalWrite(Motor_L_pwm_pin, HIGH);
-  digitalWrite(Motor_R_dir_pin, LOW);
-  digitalWrite(Motor_R_pwm_pin, HIGH);
-
-  if (distance_L<=0) {
-    digitalWrite(Motor_L_dir_pin, LOW);
-    digitalWrite(Motor_L_pwm_pin, LOW);
-    digitalWrite(Motor_R_dir_pin, LOW);
-    digitalWrite(Motor_R_pwm_pin, LOW);
-    }
-
-  if (Serial.available() > 0){                                                          //ex3
-    String message = Serial.readStringUntil('\n'); 
-    Serial.print("Message received, content: ");  
-    Serial.println(message);
-    int pos_s = message.indexOf("LCD");
-    lcd.setCursor(0, 0);
-    lcd.clear();
-
-    if (pos_s > -1){
-      Serial.println("Command = LCD ");
-      pos_s = message.indexOf(":");
-
-      if (pos_s > -1){
-        String stat = message.substring(pos_s + 1);
-        lcd.print(stat);
-      }  
-    }
-    else{
-      lcd.print("No message found");
-      Serial.println("No message found, try typing LCD:text\n");
-    }
-  }
-
+    count_L = 0;
     
 }
 
+
 void isr_L() {
-  if(yd>0)
-  {
     count_L++;
-    }else {
-      count_L--;
-    }
 }
 
 void isr_R() {
-  if(yd>0)
-  {
     count_R++;
-    }else {
-      count_R--;
-    }
 }
+
+                          
+  
+  
